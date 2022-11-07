2c9952f6748034baff1e253508dff0584a2f9366
Alastair <alastair.chau@gmail.com>
2022-11-07 08:01:19 +0000

    test coverage stats added

--- Gemfile
+++ Gemfile
@@ -16,3 +16,7 @@ gem "rack-test", "~> 2.0"
 gem "bcrypt", "~> 3.1"
 
 gem "sinatra-flash", "~> 0.3.0"
+
+gem "simplecov", "~> 0.21.2"
+
+gem "simplecov-console", "~> 0.9.1"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
+require 'simplecov'
+require 'simplecov-console'
 require 'database_connection'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+
 
 ENV['ENV'] = 'test'
 
